404978759e85bf76923c48a582f92e0669face3e
Alaikum <105469969+Alaikum@users.noreply.github.com>
2023-01-24 15:57:32 +0100

    Update Bonus.java

--- hello-java/src/org/lessons/java/Bonus.java
+++ hello-java/src/org/lessons/java/Bonus.java
@@ -5,8 +5,15 @@ public class Bonus {
 		System.out.println("Benvenuto al primo progetto, Bonus-Java");
 		String name = "Adamo";
 		String last_name = "Carangi";
-		System.out.println("Nome Concatenato "+name + " " + last_name);
-		String full_name= name+" "+last_name;
+		System.out.println("Nome Concatenato " + name + " " + last_name);
+		String full_name = name + " " + last_name;
 		System.out.println("Nome Completo " + full_name);
+		// altre variabili
+		int num = 5;
+		float floatNum = 15.99f;
+		char letter = 'A';
+		boolean bool = true;
+		System.out.println(num + " " + floatNum + " " + letter + " " + bool + " ");
+
 	}
 }
