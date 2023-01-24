be47b7136b415a8b82b29722bcdb52b19051cf52
Alaikum <105469969+Alaikum@users.noreply.github.com>
2023-01-24 15:47:14 +0100

    Add files via upload

---
+++ hello-java/src/org/lessons/java/Bonus.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+public class Bonus {
+	public static void main(String[] args) {
+		System.out.println("Benvenuto al primo progetto, Bonus-Java");
+		String name = "Adamo";
+		String last_name = "Carangi";
+		System.out.println("Nome Concatenato "+name + " " + last_name);
+		String full_name= name+" "+last_name;
+		System.out.println("Nome Completo " + full_name);
+	}
+}
