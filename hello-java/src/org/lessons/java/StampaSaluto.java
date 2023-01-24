3b37e312dd6ebfb5a4029f9a70df9729a0d13fa9
Alaikum <105469969+Alaikum@users.noreply.github.com>
2023-01-24 15:32:32 +0100

    Add files via upload

---
+++ hello-java/src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class StampaSaluto {
+	public static void main(String[] args) {
+		System.out.println("Benvenuto al primo progetto Java");
+		
+	}
+}
